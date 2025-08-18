b4ea8f288218eb992b4177a03a6040f60dea3432
limon4ik-black <evgesha.lim04@mail.ru>
2025-08-18 20:58:36 +0300

    consumer

--- internal/kafka/init_kafka.go
+++ internal/kafka/init_kafka.go
@@ -2,7 +2,9 @@ package kafka
 
 import (
 	"context"
+	"encoding/json"
 	"log/slog"
+	"order-service/internal/models"
 
 	"github.com/segmentio/kafka-go"
 )
@@ -24,11 +26,32 @@ func StartConsumer(reader *kafka.Reader, log *slog.Logger) {
 			log.Error("Kafka read error", "error", err)
 			continue
 		}
-		log.Info("Kafka message", "value", string(m.Value))
+		//log.Info("Kafka message", "value", string(m.Value))
 
+		//valid
 		// ПАРС
+
+		orderStruct, err := ParseMessage(m, log)
+		if err != nil {
+			continue
+		}
+		log.Info("OMG"+orderStruct.Order_uid+orderStruct.Customer_id, orderStruct.Delivery.Name, orderStruct.Delivery.Zip)
+		//...
 		// ЗАЛИВ В БД
 		// ЗАЛИВ В КЕШ
 
 	}
+}
+
+func ParseMessage(m kafka.Message, log *slog.Logger) (*models.Order, error) {
+
+	var order models.Order
+	err := json.Unmarshal(m.Value, &order)
+
+	if err != nil {
+		log.Error("failed to parse json", "error", err)
+		return nil, err
+	}
+
+	return &order, nil
 }
--- internal/models/struct.go
+++ internal/models/struct.go
@@ -1,20 +1,22 @@
 package models
 
+import "time"
+
 type Order struct {
-	Order_uid          string   `json:"order_uid"`
-	Track_number       string   `json:"track_number"`
-	Entry              string   `json:"entry"`
-	Delivery           Delivery `json:"delivery"`
-	Payment            Payment  `json:"payment"`
-	Items              []Item   `json:"items"`
-	Locale             string   `json:"locale"`
-	Internal_signature string   `json:"internal_signature"`
-	Customer_id        string   `json:"customer_id"`
-	Delivery_service   string   `json:"delivery_string"`
-	Shard_key          string   `json:"shard_key"`
-	Sm_id              int      `json:"sm_key"`
-	Date_created       string   `json:"date_created"`
-	Oof_shard          string   `json:"oof_shard"`
+	Order_uid          string    `json:"order_uid"`
+	Track_number       string    `json:"track_number"`
+	Entry              string    `json:"entry"`
+	Delivery           Delivery  `json:"delivery"`
+	Payment            Payment   `json:"payment"`
+	Items              []Item    `json:"items"`
+	Locale             string    `json:"locale"`
+	Internal_signature string    `json:"internal_signature"`
+	Customer_id        string    `json:"customer_id"`
+	Delivery_service   string    `json:"delivery_service"`
+	Shard_key          string    `json:"shard_key"`
+	Sm_id              int       `json:"sm_id"`
+	Date_created       time.Time `json:"date_created"`
+	Oof_shard          string    `json:"oof_shard"`
 }
 
 type Delivery struct {
